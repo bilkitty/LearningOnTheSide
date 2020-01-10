6c8b764cbe6fd7d6ddb76e18f1031791a374ad73
bilkitty <githinjibcw@hotmail.com>
2020-01-10 11:19:14 -0500

    Make sure all test methods are prefixed with test_... -_-.

--- qlearning/tests/unittests.py
+++ qlearning/tests/unittests.py
@@ -4,26 +4,27 @@ from qlearning import *
 from environments import EnvTypes, ENV_DICTIONARY
 
 TEST_QTABLE_PKL = "test.pkl"
-# TODO: no idea why these are not running... followed doc almost exactly...
+# TODO: no idea why these are not running... followed doc almost exactly
+# You have to prefix each method with "test_"
 
 class TestEnvironmentCreation(unittest.TestCase):
-    def CreateWindyGrid(self):
+    def test_CreateWindyGrid(self):
         self.assertNotEqual(ENV_DICTIONARY[EnvTypes.WindyGridEnv](), None)
 
-    def CreateTaxiGrid(self):
+    def test_CreateTaxiGrid(self):
         self.assertNotEqual(ENV_DICTIONARY[EnvTypes.TaxiGridEnv](), None)
 
-    def CreateCartPole(self):
+    def test_CreateCartPole(self):
         self.assertNotEqual(ENV_DICTIONARY[EnvTypes.CartPoleEnv](), None)
 
 
 class TestQlearningSteps(unittest.TestCase):
-    def SingleTrainCycle(self):
+    def test_SingleTrainCycle(self):
         # check results are available
         # check success
         self.assertTrue(False)
 
-    def SingleEvaluationCycle(self):
+    def test_SingleEvaluationCycle(self):
         # load qtable
         # check results are available
         # check success
