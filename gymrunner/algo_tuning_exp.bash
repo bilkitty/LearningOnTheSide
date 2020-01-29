6d5552326d7f271b8c45d0ef62db9cd760923470
bilkitty <githinjibcw@hotmail.com>
2020-01-29 13:49:47 -0500

    udpate configfile drop loc.

--- gymrunner/algo_tuning_exp.bash
+++ gymrunner/algo_tuning_exp.bash
@@ -6,25 +6,26 @@ algos=(Qlearning ddpg)
 # Run mix of discrete and continuous environements
 # TEMP: don't run discrete envs
 algoType=1
-resultsdir="../data/results/algo_tuning/working"
+resultsdir="../data/results/DDPG/postrefactor"
 configdir="config"
 
 # Try low explore rate
-for config in ddpg_ace_pendulum_h256b128d99 h64b128d99s01 h256b128d99s30 h256b128d50s01
+for config in h256b128d99s01 h256b128d99s01 h256b128d99s30 h64b128d99s30
 do
   echo "Processing $config"
   trialdir="$resultsdir"
   mkdir -p $trialdir
-  for env in 5 6
+  for env in 6
   do
+    trialfilename=$trialdir/${algos[$algoType]}_${names[$env]}_$config
     cp "$configdir/$config.json" "params.json"
     echo "Running ${algos[$algoType]} in ${names[$env]}"
-    python3 main.py --envIndex $env --algoIndex $algoType > "$trialdir/$config.txt"
+    python3 main.py --envIndex $env > "$trialfilename.txt"
     if [ $? -eq 0 ]
     then
       mv *.png $trialdir
       mv *.pkl $trialdir
-      cp "params.json" "$trialdir/$config.json"
+      cp "params.json" "$trialfilename.json"
     else
       echo "Failed with exit code: $?"
     fi
